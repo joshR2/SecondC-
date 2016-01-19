927f02a1eb2764fd0bd989814ac7313d23ee2ae5
joshR2 <josh2cool365@aol.com>
2016-01-19 09:47:24 -0700

    Created My Second C++ Project.

---
+++ src/Controller/SillyAppController.cpp
@@ -0,0 +1,35 @@
+/*
+ * SillyAppController.cpp
+ *
+ *  Created on: Jan 19, 2016
+ *      Author: jrob2049
+ */
+
+#include "SillyAppController.h"
+#include <iostream>
+using namespace std;
+
+SillyAppController :: SillyAppController()
+{
+this->count = 99;
+}
+
+void SillyAppController :: setCount(int count)
+{
+	this->count = count;
+}
+int SillyAppController :: getCount()
+{
+	return this->count;
+}
+
+void SillyAppController :: start()
+{
+cout << "In the SillyAppControlles\'s start method" <<endl;
+cout << getCount() << " is the count right now" << endl;
+cout << "Type in a new value for count please" << endl;
+int tempCount;
+cin >> tempCount;
+setCount(tempCount);
+cout << getCount() << " is the updated count" << endl;
+}
